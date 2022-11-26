fe6dcb57a98e3e45e575b5f776a0dae173fb0427
Alex Ehlke <alex.ehlke@gmail.com>
2022-11-26 12:12:02 +0900

    location

--- Sources/LakeKit/LocationBar.swift
+++ Sources/LakeKit/LocationBar.swift
@@ -7,32 +7,47 @@ public class LocationController: ObservableObject {
     public init() { }
 }
 
-private struct LocationTextFieldModifier: ViewModifier {
-    var text: String
+//private struct LocationTextFieldModifier: ViewModifier {
+//    var text: String
+//    
+//    func body(content: Content) -> some View {
+//        content
+//        // Clear color for the TextField
+//            .foregroundColor(.clear)
+//        // Overlay with text and extra
+//            .overlay(
+//                HStack(spacing: 0.0) {
+//                    // This Swift View splits the text and adds what I need
+//                    //                    TextFieldHighlightedVariables(text)
+//                    //                    Text
+//                    Text("    uh    :)")
+//                    Spacer()
+//                }
+//                    .padding(.top, 2)
+//                    .padding(.leading, 4)
+//                ,
+//                alignment: .topLeading
+//            )
+//    }
+//}
+
+public enum LocationBarAction: Equatable {
+    case idle
+    case update(String)
     
-    func body(content: Content) -> some View {
-        content
-        // Clear color for the TextField
-            .foregroundColor(.clear)
-        // Overlay with text and extra
-            .overlay(
-                HStack(spacing: 0.0) {
-                    // This Swift View splits the text and adds what I need
-                    //                    TextFieldHighlightedVariables(text)
-                    //                    Text
-                    Text("    uh    :)")
-                    Spacer()
-                }
-                    .padding(.top, 2)
-                    .padding(.leading, 4)
-                ,
-                alignment: .topLeading
-            )
+    public static func == (lhs: LocationBarAction, rhs: LocationBarAction) -> Bool {
+        if case .idle = lhs, case .idle = rhs {
+            return true
+        }
+        if case .update(let lhsLocation) = lhs, case .update(let rhsLocation) = rhs {
+            return lhsLocation == rhsLocation
+        }
+        return false
     }
 }
 
 public struct LocationBar: View, Equatable {
-//    let loadLocation: String?
+    @Binding var action: LocationBarAction
     @State private var locationText = ""
     private let onSubmit: ((URL?, String) -> Void)
     @EnvironmentObject private var locationController: LocationController
@@ -47,9 +62,16 @@ public struct LocationBar: View, Equatable {
     }
     
     public var body: some View {
-        return TextField("Search or enter website address", text: $locationText)
+        TextField("Search or enter website address", text: $locationText)
             .truncationMode(.tail)
+        #if os(macOS)
             .textFieldStyle(.roundedBorder)
+        #else
+            .textFieldStyle(.plain)
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 9, trailing: 10))
+            .background(Color.secondary.opacity(0.2232))
+            .cornerRadius(8)
+        #endif
             .submitLabel(.go)
             .onSubmit {
                 onSubmit(url, locationText)
@@ -74,29 +96,21 @@ public struct LocationBar: View, Equatable {
                     textField.resignFirstResponder()
                 }
             }
-//            .onChange(of: loadLocation) { newLocation in
-//                guard let loadLocation = loadLocation else { return }
-//                locationText = loadLocation
-//                onSubmit(url, locationText)
-//            }
-            .onChange(of: locationController.isPresentingLocationOpening) { isPresentingLocationOpening in
+            .onChange(of: action) { action in
+                DispatchQueue.main.async {
+                    switch action {
+                    case .update(let newLocationTexts):
+                        locationText = newLocationTexts
+                        self.action = .idle
+                    case .idle:
+                        break
+                    }
+                }
             }
-        //                .focused($isFocused)
-        //                .padding(EdgeInsets(top: 0, leading: , bottom: 0, trailing: 6))
-        //                .cornerRadius(5)
-        //                .overlay(
-        //                    RoundedRectangle(cornerRadius: 5)
-        //                        .stroke(lineWidth: 1.0)
-        //                )
-        //                .opacity(isFocused ? 1 : 0.5)
-        //        }
-        //                .frame(width: min(1, geo.size.width - 90))
-        //                    .frame(maxWidth: .infinity)
-        //                    .fixedSize(horizontal: true, vertical: false)
     }
     
-    public init(loadLocation: String?, onSubmit: @escaping ((URL?, String) -> Void)) {
-        _locationText = State(initialValue: loadLocation ?? "")
+    public init(action: Binding<LocationBarAction>, onSubmit: @escaping ((URL?, String) -> Void)) {
+        _action = action
         self.onSubmit = onSubmit
     }
     
