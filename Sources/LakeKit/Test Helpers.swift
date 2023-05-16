cee3d429ae3b563ad6c059c47820e1096e007d2a
Alex Ehlke <alex.ehlke@gmail.com>
2023-05-16 19:50:46 -0400

    wip

--- Sources/LakeKit/Test Helpers.swift
+++ Sources/LakeKit/Test Helpers.swift
@@ -1,3 +1,4 @@
+#if TEST
 import Foundation
 import XCTest
 
@@ -6,12 +7,14 @@ public protocol Screen {
 }
 
 public extension Screen {
-    func takeScreenshot(_ name: String) {
+    func takeScreenshot(_ name: String) -> Self {
         XCTContext.runActivity(named: name) { activity in
             let screenshot = app.screenshot()
             let attachment = XCTAttachment(screenshot: screenshot)
             attachment.lifetime = .keepAlways
             activity.add(attachment)
         }
+        return self
     }
 }
+#endif
