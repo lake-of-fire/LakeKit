505fccbe63243cc4504e53bfa2054a16910fe72d
Alex Ehlke <alex.ehlke@gmail.com>
2023-05-17 17:23:23 -0400

    wip

--- Sources/LakeKit/Test Helpers.swift
+++ Sources/LakeKit/Test Helpers.swift
@@ -1,20 +1,20 @@
-#if canImport(XCTest)
-import Foundation
-import XCTest
-
-public protocol Screen {
-    var app: XCUIApplication { get }
-}
-
-public extension Screen {
-    func takeScreenshot(_ name: String) -> Self {
-        XCTContext.runActivity(named: name) { activity in
-            let screenshot = app.screenshot()
-            let attachment = XCTAttachment(screenshot: screenshot)
-            attachment.lifetime = .keepAlways
-            activity.add(attachment)
-        }
-        return self
-    }
-}
-#endif
+//#if TEST
+//import Foundation
+//import XCTest
+//
+//public protocol Screen {
+//    var app: XCUIApplication { get }
+//}
+//
+//public extension Screen {
+//    func takeScreenshot(_ name: String) -> Self {
+//        XCTContext.runActivity(named: name) { activity in
+//            let screenshot = app.screenshot()
+//            let attachment = XCTAttachment(screenshot: screenshot)
+//            attachment.lifetime = .keepAlways
+//            activity.add(attachment)
+//        }
+//        return self
+//    }
+//}
+//#endif
