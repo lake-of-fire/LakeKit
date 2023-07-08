acefc14bac2289d8bfa73583580f3e4c79ec317b
Alex Ehlke <alex.ehlke@gmail.com>
2023-07-07 23:18:17 -0400

    wip

--- Sources/LakeKit/String+Truncation.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension String {
-    func truncate(_ length: Int, trailing: String? = "…") -> String {
-        return (self.count > length) ? self.prefix(length) + (trailing ?? "") : self
-    }
-}
