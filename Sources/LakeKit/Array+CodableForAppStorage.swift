aab991e82044295080aa3307267814d0f8cac070
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-10 00:24:39 -0400

    wip

---
+++ Sources/LakeKit/Array+CodableForAppStorage.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+// From https://gist.github.com/thanakijwanavit/6ea944bb1b38c85d98096e0328b5d23b
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
