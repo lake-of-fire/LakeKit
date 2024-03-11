033dfde3b7b14bab64751f825a790eda6fdb2d27
Alex Ehlke <alex.ehlke@gmail.com>
2024-03-11 19:30:31 -0400

    wip

--- Sources/LakeKit/LRUCache/LRUFileCache.swift
+++ Sources/LakeKit/LRUCache/LRUFileCache.swift
@@ -4,11 +4,11 @@ import SwiftUtilities
 
 open class LRUFileCache<I: Encodable, O: Codable>: ObservableObject {
     @Published public var cacheDirectory: URL
-    private let cache: LRUCache<UInt64, O>
+    private let cache: LRUCache<UInt64, Any?>
     
     private lazy var jsonEncoder: JSONEncoder = {
         let encoder = JSONEncoder()
-        encoder.outputFormatting = .sortedKeys
+        encoder.outputFormatting = [.sortedKeys, .withoutEscapingSlashes]
         return encoder
     }()
     
@@ -36,55 +36,61 @@ open class LRUFileCache<I: Encodable, O: Codable>: ObservableObject {
     }
     
     private func cacheURL(forKeyHash keyHash: UInt64) -> URL {
-        return cacheDirectory.appendingPathComponent("hash-" + String(keyHash)).appendingPathExtension("json")
+        return cacheDirectory.appendingPathComponent("hash-" + String(keyHash)).appendingPathExtension(O.self == String.self ? "lzfse" : "json")
     }
     
     private func cacheKeyHash(_ key: I) -> UInt64? {
         guard let data = try? jsonEncoder.encode(key) else { return nil }
         let hash = stableHash(data: data)
-        print("!!! \(key) === \(String(data: data, encoding: .utf8) ?? "") === \(hash)")
         return hash
     }
     
     public func value(forKey key: I) -> O? {
         guard let hash = cacheKeyHash(key) else { return nil }
-        print("!! get \(cache.value(forKey: hash).debugDescription.prefix(100)) for hash \(hash) key \(String(describing: key).prefix(100))")
-        return cache.value(forKey: hash)
+        let value = cache.value(forKey: hash)
+        if O.self == String.self, let value = value as? Data, let decompressedData = try? (value as NSData).decompressed(using: .lzfse) as Data {
+            return String(data: decompressedData, encoding: .utf8) as? O
+        }
+        return value as? O
     }
     
     public func setValue(_ value: O?, forKey key: I) {
         guard let keyHash = cacheKeyHash(key) else { return }
         let url = cacheURL(forKeyHash: keyHash)
-        if let value = value {
-            if let encoded = try? jsonEncoder.encode(value) {
-                do {
+        do {
+            if let value = value {
+                if let stringValue = value as? String, let compressed = try? (stringValue.data(using: .utf8) as? NSData)?.compressed(using: .lzfse) as? Data {
+                    try compressed.write(to: url, options: .atomic)
+                    cache.setValue(compressed, forKey: keyHash, cost: compressed.count)
+                } else if let encoded = try? jsonEncoder.encode(value) {
                     try encoded.write(to: url, options: .atomic)
-                } catch {
-                    print(error)
+                    cache.setValue(value, forKey: keyHash, cost: encoded.count)
                 }
-                print("!! set \(String(describing: value).prefix(100)) for hash \(keyHash) key \(String(describing: key).prefix(100))")
-                cache.setValue(value, forKey: keyHash, cost: encoded.count)
+            } else {
+                try FileManager.default.removeItem(at: url)
+                cache.removeValue(forKey: keyHash)
             }
-        } else {
-            try? FileManager.default.removeItem(at: url)
-            cache.removeValue(forKey: keyHash)
+        } catch {
+            print(error)
         }
         
         try? deleteOrphanFiles()
     }
     
     private func rebuild() {
-        DispatchQueue.global().async { [weak self] in
-            guard let self = self else { return }
-            let fileManager = FileManager.default
-            if let contents = try? fileManager.contentsOfDirectory(at: self.cacheDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles) {
-                for item in contents where item.pathExtension == "json" {
-                    if let data = try? Data(contentsOf: item), let decodedValue = try? JSONDecoder().decode(O.self, from: data), let keyHash = UInt64(item.deletingPathExtension().lastPathComponent) {
-                        cache.setValue(decodedValue, forKey: keyHash, cost: data.count)
-                    }
+        let fileManager = FileManager.default
+        if let contents = try? fileManager.contentsOfDirectory(at: self.cacheDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles) {
+            for item in contents where item.pathExtension == "lzfse" {
+                if let data = try? Data(contentsOf: item), let keyHash = UInt64(item.deletingPathExtension().lastPathComponent.dropFirst("hash-".count)) {
+                    cache.setValue(data, forKey: keyHash, cost: data.count)
                 }
-                try? deleteOrphanFiles()
             }
+            for item in contents where item.pathExtension == "json" {
+                if let data = try? Data(contentsOf: item), let decodedValue = try? JSONDecoder().decode(O.self, from: data), let keyHash = UInt64(item.deletingPathExtension().lastPathComponent.dropFirst("hash-".count)) {
+                    cache.setValue(decodedValue, forKey: keyHash, cost: data.count)
+                }
+            }
+            try? deleteOrphanFiles()
         }
     }
     
@@ -93,13 +99,13 @@ open class LRUFileCache<I: Encodable, O: Codable>: ObservableObject {
         let contents = try fileManager.contentsOfDirectory(at: cacheDirectory, includingPropertiesForKeys: nil)
         let existing = Set(cache.allKeys)
         
-        for item in contents where item.pathExtension == "json" {
+        let validExtensions = Set(["json", "lzfse"])
+        for item in contents where validExtensions.contains(item.pathExtension) {
             guard let keyHash = UInt64(String(item.deletingPathExtension().lastPathComponent.dropFirst("hash-".count))) else {
                 try fileManager.removeItem(at: item)
                 continue
             }
             if !existing.contains(keyHash) {
-                print("!! delete \(keyHash)")
                 try fileManager.removeItem(at: item)
             }
         }
