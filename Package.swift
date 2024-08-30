9ed548ef77a99d16dae1f829b81c9e0ce51669ca
Alex Ehlke <alex.ehlke@gmail.com>
2024-08-30 00:15:53 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -1,15 +1,16 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LakeKit",
-    platforms: [.macOS(.v12), .iOS(.v15)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LakeKit",
+            type: .dynamic,
             targets: ["LakeKit"]),
     ],
     dependencies: [
