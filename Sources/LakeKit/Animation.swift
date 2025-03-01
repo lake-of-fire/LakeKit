8ad3064e9442ff392119d914855dc54693a91d4a
Alex Ehlke <alex.ehlke@gmail.com>
2025-03-01 02:57:19 -0500

    wip

--- Sources/LakeKit/Animation.swift
+++ Sources/LakeKit/Animation.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
-public extension View {
-    // From: https://stackoverflow.com/questions/69712759/swiftui-fullscreencover-with-no-animation
-    func withoutAnimation(action: @escaping () -> Void) {
-        var transaction = Transaction()
-        transaction.disablesAnimations = true
-        withTransaction(transaction) {
-            action()
-        }
+// From: https://stackoverflow.com/questions/69712759/swiftui-fullscreencover-with-no-animation
+public func withoutAnimation(action: @escaping () -> Void) {
+    var transaction = Transaction()
+    transaction.disablesAnimations = true
+    withTransaction(transaction) {
+        action()
     }
 }
