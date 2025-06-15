de59d920961654c42b07661df4ddfb7f30f8f68d
Alex Ehlke <alex.ehlke@gmail.com>
2025-06-14 20:46:48 -0400

    wip

---
+++ Sources/LakeKit/ClearBorderedButtonStyle.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+public struct ClearBorderedButtonStyle: ButtonStyle {
+    @Environment(\.controlSize) private var controlSize
+    
+#if os(iOS)
+    private var miniMinWidth: CGFloat {
+        smallMinWidth
+    }
+    private var miniMinHeight: CGFloat {
+        smallMinHeight
+    }
+#elseif os(macOS)
+    @ScaledMetric private var miniMinWidth: CGFloat = 32
+    @ScaledMetric private var miniMinHeight: CGFloat = 32
+#endif
+    
+    @ScaledMetric private var smallMinWidth: CGFloat = 38
+    @ScaledMetric private var smallMinHeight: CGFloat = 38
+    
+    @ScaledMetric private var regularMinWidth: CGFloat = 44
+    @ScaledMetric private var regularMinHeight: CGFloat = 44
+    
+    @ScaledMetric private var largeMinWidth: CGFloat = 52
+    @ScaledMetric private var largeMinHeight: CGFloat = 52
+    
+    @ScaledMetric private var extraLargeMinWidth: CGFloat = 60
+    @ScaledMetric private var extraLargeMinHeight: CGFloat = 60
+    
+    private var minWidth: CGFloat {
+        switch controlSize {
+        case .mini: return miniMinWidth
+        case .small: return smallMinWidth
+        case .regular: return regularMinWidth
+        case .large: return largeMinWidth
+        case .extraLarge: return extraLargeMinWidth
+        default: return regularMinWidth
+        }
+    }
+    
+    private var minHeight: CGFloat {
+        switch controlSize {
+        case .mini: return miniMinHeight
+        case .small: return smallMinHeight
+        case .regular: return regularMinHeight
+        case .large: return largeMinHeight
+        case .extraLarge: return extraLargeMinHeight
+        default: return regularMinHeight
+        }
+    }
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            configuration.label
+        }
+        .background(.white.opacity(0.0000000001))
+        .frame(minWidth: minWidth, minHeight: minHeight)
+    }
+}
+
+public extension ButtonStyle where Self == ClearBorderedButtonStyle {
+    static var clearBordered: Self { Self() }
+}
