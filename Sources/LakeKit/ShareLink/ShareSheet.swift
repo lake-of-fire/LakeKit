23635c33ceced7e37e6a976238e723e03781f159
Alex Ehlke <alex.ehlke@gmail.com>
2026-04-18 20:40:57 -0400

    Align share links with transferable changes

--- Sources/LakeKit/ShareLink/ShareSheet.swift
+++ Sources/LakeKit/ShareLink/ShareSheet.swift
@@ -1,92 +1,90 @@
 import SwiftUI
+#if os(macOS)
+import AppKit
+#endif
 //import SwiftBackports
 
-//#if os(macOS) || os(iOS)
-#if os(iOS)
+#if os(macOS) || os(iOS)
 public extension View {
     @ViewBuilder
     func shareSheet<Data>(item activityItems: Binding<ActivityItem<Data>?>) -> some View where Data: RandomAccessCollection, Data.Element: Shareable {
-//#if os(macOS)
-//        background(ShareSheet(isActive: isActive, item: activityItems))
-//#elseif os(iOS)
         background(ShareSheet(item: activityItems))
-//#endif
     }
 }
 #endif
 
 #if os(macOS)
 
-//private struct ShareSheet<Data>: NSViewRepresentable where Data: RandomAccessCollection, Data.Element: Shareable {
-//    @Binding var item: ActivityItem<Data>?
-//
-//    public func makeNSView(context: Context) -> SourceView {
-//        SourceView(item: $item)
-//    }
-//
-//    public func updateNSView(_ view: SourceView, context: Context) {
-//        view.item = $item
-//    }
-//
-//    final class SourceView: NSView, NSSharingServicePickerDelegate, NSSharingServiceDelegate {
-//        var picker: NSSharingServicePicker?
-//
-//        var item: Binding<ActivityItem<Data>?> {
-//            didSet {
-//                updateControllerLifecycle(
-//                    from: oldValue.wrappedValue,
-//                    to: item.wrappedValue
-//                )
-//            }
-//        }
-//
-//        init(item: Binding<ActivityItem<Data>?>) {
-//            self.item = item
-//            super.init(frame: .zero)
-//        }
-//
-//        required init?(coder: NSCoder) {
-//            fatalError("init(coder:) has not been implemented")
-//        }
-//
-//        private func updateControllerLifecycle(from oldValue: ActivityItem<Data>?, to newValue: ActivityItem<Data>?) {
-//            switch (oldValue, newValue) {
-//            case (.none, .some):
-//                presentController()
-//            case (.some, .none):
-//                dismissController()
-//            case (.some, .some), (.none, .none):
-//                break
-//            }
-//        }
-//
-//        func presentController() {
-//            picker = NSSharingServicePicker(items: item.wrappedValue?.data.map { $0 } ?? [])
-//            picker?.delegate = self
-//            DispatchQueue.main.async {
-//                guard self.window != nil else { return }
-//                self.picker?.show(relativeTo: self.bounds, of: self, preferredEdge: .minY)
-//            }
-//        }
-//
-//        func dismissController() {
-//            item.wrappedValue = nil
-//        }
-//
-//        func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, delegateFor sharingService: NSSharingService) -> NSSharingServiceDelegate? {
-//            return self
-//        }
-//
-//        public func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, didChoose service: NSSharingService?) {
-//            sharingServicePicker.delegate = nil
-//            dismissController()
-//        }
-//
-//        func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, sharingServicesForItems items: [Any], proposedSharingServices proposedServices: [NSSharingService]) -> [NSSharingService] {
-//            proposedServices
-//        }
-//    }
-//}
+private struct ShareSheet<Data>: NSViewRepresentable where Data: RandomAccessCollection, Data.Element: Shareable {
+    @Binding var item: ActivityItem<Data>?
+
+    public func makeNSView(context: Context) -> SourceView {
+        SourceView(item: $item)
+    }
+
+    public func updateNSView(_ view: SourceView, context: Context) {
+        view.item = $item
+    }
+
+    final class SourceView: NSView, NSSharingServicePickerDelegate, NSSharingServiceDelegate {
+        var picker: NSSharingServicePicker?
+
+        var item: Binding<ActivityItem<Data>?> {
+            didSet {
+                updateControllerLifecycle(
+                    from: oldValue.wrappedValue,
+                    to: item.wrappedValue
+                )
+            }
+        }
+
+        init(item: Binding<ActivityItem<Data>?>) {
+            self.item = item
+            super.init(frame: .zero)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        private func updateControllerLifecycle(from oldValue: ActivityItem<Data>?, to newValue: ActivityItem<Data>?) {
+            switch (oldValue, newValue) {
+            case (.none, .some):
+                presentController()
+            case (.some, .none):
+                dismissController()
+            case (.some, .some), (.none, .none):
+                break
+            }
+        }
+
+        func presentController() {
+            picker = NSSharingServicePicker(items: item.wrappedValue?.data.map(\.sharingItem) ?? [])
+            picker?.delegate = self
+            DispatchQueue.main.async {
+                guard self.window != nil else { return }
+                self.picker?.show(relativeTo: self.bounds, of: self, preferredEdge: .minY)
+            }
+        }
+
+        func dismissController() {
+            item.wrappedValue = nil
+        }
+
+        func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, delegateFor sharingService: NSSharingService) -> NSSharingServiceDelegate? {
+            self
+        }
+
+        public func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, didChoose service: NSSharingService?) {
+            sharingServicePicker.delegate = nil
+            dismissController()
+        }
+
+        func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, sharingServicesForItems items: [Any], proposedSharingServices proposedServices: [NSSharingService]) -> [NSSharingService] {
+            proposedServices
+        }
+    }
+}
 
 #elseif os(iOS)
 
@@ -140,7 +138,7 @@ private extension ShareSheet {
         }
 
         private func presentController() {
-            let controller = UIActivityViewController(activityItems: item.wrappedValue?.data.map { $0 } ?? [], applicationActivities: nil)
+            let controller = UIActivityViewController(activityItems: item.wrappedValue?.data.map(\.sharingItem) ?? [], applicationActivities: nil)
             controller.presentationController?.delegate = self
 //            controller.popoverPresentationController?.permittedArrowDirections = .any
             controller.popoverPresentationController?.permittedArrowDirections = []
