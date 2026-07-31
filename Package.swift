92c729c4cb65e7d82aaf508e49afd48480efe059
Alex Ehlke <alex.ehlke@gmail.com>
2026-07-31 17:16:08 -0400

    Checkpoint concurrent LakeKit package configuration

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LakeKit",
-//            type: .dynamic,
+//            type: .static,
             targets: ["LakeKit"]),
     ],
     dependencies: [
